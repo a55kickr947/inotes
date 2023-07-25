c240c5c1d0a534ea0b5ae4727aed5672f03cfca9
a55kickr947 <sumanths947@gmail.com>
2023-07-25 09:48:08 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require ('mongoose');
+const mongoURI = 'mongodb://127.0.0.1:27017'
+
+const connectTomongo = async () => {
+    try {
+      await mongoose.connect(mongoURI);
+      console.log("Connected to the database successfully");
+    } catch (error) {
+      console.error("Error connecting to the database:", error);
+    }
+  };
+  
+module.exports =connectTomongo;
