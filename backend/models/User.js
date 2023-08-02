33b350ffaca3980e4eeb7cc5fe92754a780cadbe
a55kickr947 <sumanths947@gmail.com>
2023-08-02 22:12:19 +0530

    3rd commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,23 +1,24 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const UserSchema = new Schema(
-  {
-    name: {
-      required: true,
-      type: String,
-    },
-    password: {
-      required: true,
-      type: String,
-    },
-    email: {
-      required: true,
-      type: String,
-      unique: true,
-    },
-  },
-  { timestamps: true }
-);
 
-
-module.exports = mongoose.model("user", UserSchema);
+const UserSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    },
+  });
+  const User = mongoose.model('user', UserSchema);
+  module.exports = User;
